7f82107df5de7633e93a060951a91535a41ad43b
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-08-16 20:48:58 +0900

    ヘッダーの作成 ・ヘッダーページ作成 ・Profileの設定 ・Rubyのバージョン変更

---
+++ lib/pages/home_page/home_header.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeHeader extends StatelessWidget {
+  const HomeHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Welcome, Taro.',
+            style: GoogleFonts.montserrat(
+                fontSize: 28,
+                fontWeight: FontWeight.w600
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Summer Collection 2022 is now available.',
+              style: GoogleFonts.montserrat(
+                  fontSize: 16
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
